e7fda8562e8d9b9f8ae5d7ede174d1f6f5fba7cf
baiyunpeng04 <baiyunpeng04@meituan.com>
2019-05-19 13:37:29 +0800

    Need to finish

---
+++ src/main/java/com/sapphire/leetcode/contest/contest137/LongestStringChain.java
@@ -0,0 +1,14 @@
+package com.sapphire.leetcode.contest.contest137;
+
+import java.util.List;
+
+/**
+ * Author: 柏云鹏
+ * Date: 2019/5/19.
+ */
+public class LongestStringChain {
+    public int longestStrChain(String[] words) {
+        //TODO finsih the contest
+        return 0;
+    }
+}
