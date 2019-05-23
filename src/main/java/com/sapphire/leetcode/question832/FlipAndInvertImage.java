fd15a3c2ec84e0ae3331ea980c609a360eceb9c6
baiyunpeng04 <baiyunpeng04@meituan.com>
2019-05-23 23:24:38 +0800

    Add solutions

---
+++ src/main/java/com/sapphire/leetcode/question832/FlipAndInvertImage.java
@@ -0,0 +1,22 @@
+package com.sapphire.leetcode.question832;
+
+/**
+ * Author: 柏云鹏
+ * Date: 2019/5/23.
+ */
+public class FlipAndInvertImage {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for (int[] array : A) {
+            for (int i = 0; i < array.length / 2; i++) {
+                int temp = array[i];
+                array[i] = array[array.length - i - 1];
+                array[array.length - i - 1] = temp;
+            }
+
+            for (int i = 0; i < array.length; i++) {
+                array[i] = array[i] == 0 ? 1 : 0;
+            }
+        }
+        return A;
+    }
+}
