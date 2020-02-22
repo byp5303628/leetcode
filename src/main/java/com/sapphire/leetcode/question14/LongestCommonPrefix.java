978a488dedf31be88a59645171f8e025f3a29e90
baiyunpeng04 <baiyunpeng04@meituan.com>
2020-02-22 18:03:08 +0800

    Add impl

---
+++ src/main/java/com/sapphire/leetcode/question14/LongestCommonPrefix.java
@@ -0,0 +1,20 @@
+package com.sapphire.leetcode.question14;
+
+/**
+ * Author: 柏云鹏
+ * Date: 2020/2/22.
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String str = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(str) != 0) {
+                str = str.substring(0, str.length() - 1);
+            }
+        }
+        return str;
+    }
+}
